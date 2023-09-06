5f7234d0f36075db1daf38cfc5616bfd9a4be104
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-09-06 14:48:40 +0200

    accessibility fixes, visual adjustments

--- index.js
+++ index.js
@@ -1,58 +1,43 @@
 class Rating {
-    constructor(){
-        this.rating = -1;
-        this.prev_target = null
-    }
+  constructor() {
+    this.rating = -1;
+  }
 
-    setScore = (event) => {
-        if(this.prev_target != null) {
-            this.prev_target.classList.toggle('selected')
-        }
-        this.rating = parseInt(event.target.innerText)
-        event.target.classList.toggle('selected')
-        this.prev_target = event.target
-    }
+  setScore = (event) => {
+    console.log(event.target);
+    this.rating = parseInt(event.target.id);
+  };
 
-    submitScore = () => {
-        if(this.rating == -1) return console.log('Please select a score');
+  submitScore = () => {
+    if (this.rating == -1) return console.log("Please select a score");
 
-        document.getElementsByClassName("user_rating")[0].innerText = this.rating
+    document.getElementsByClassName("user_rating")[0].innerText = this.rating;
 
+    rating_div.classList.replace("visible", "hidden");
+    thank_you_div.classList.replace("hidden", "visible");
 
-        rating_div.classList.replace('visible', 'hidden')
-        thank_you_div.classList.replace('hidden', 'visible')
+    //do stuff with rating
+    console.log(this.rating);
 
-        //do stuff with rating
-        console.log(this.rating)
-
-
-        //reset rating
-        this.prev_target.classList.toggle('selected')
-        this.prev_target = null
-        this.rating = -1
-    }
+    //reset rating
+    this.rating = -1;
+  };
 }
 
-const rating = new Rating()
+const rating = new Rating();
 
-const rating_div = document.getElementsByClassName('rating')[0]
-const thank_you_div = document.getElementsByClassName('thank_you')[0]
-const scores = document.getElementsByClassName('score')
+const rating_div = document.getElementsByClassName("rating")[0];
+const thank_you_div = document.getElementsByClassName("thank_you")[0];
+const scores = document.getElementsByClassName("score_num");
 
-thank_you_div.addEventListener('click', () => {
-    thank_you_div.classList.replace('visible', 'hidden')
-})
+document
+  .getElementsByClassName("submit_button")[0]
+  .addEventListener("click", rating.submitScore);
 
-document.getElementById('summon').addEventListener('click', () => {
-    rating_div.classList.replace('hidden', 'visible')
-})
-
-document.getElementsByClassName('close_button')[0].addEventListener('click', () => {
-    rating_div.classList.replace('visible', 'hidden')
-})
-
-document.getElementsByClassName('submit_button')[0].addEventListener('click', rating.submitScore)
-
-for(let score of scores) {
-    score.addEventListener('click', rating.setScore)
+for (let score of scores) {
+  score.addEventListener("click", rating.setScore);
+  score.addEventListener(
+    "keypress",
+    (e) => e.key == "Enter" && rating.submitScore()
+  );
 }
